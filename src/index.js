543f0288fe5e69f991e51dd2589cef6cb6eb5ff3
Jean-Hugues Gilbert <j.gilbert@arkadin.com>
2018-08-16 17:45:58 +0900

    v0.1.1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import BusManager from "./bus";
 import StateManager from "./state";
 
-export default {
-    BusManager: BusManager,
-    StateManager: StateManager
-};
-
 export {
     BusManager,
     StateManager
