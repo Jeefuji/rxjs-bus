8500598993c9dc7ec99a87bbc66aa4856806ebef
Jean-Hugues Gilbert <j.gilbert@arkadin.com>
2018-08-17 15:30:43 +0900

    update keywords

--- test/bus.test.js
+++ test/bus.test.js
@@ -1,7 +1,7 @@
 import './common.test'
 import uuid from "uuid/v4";
 import _ from "lodash";
-import { BusManager } from '../lib/rxjs-bus';
+import { BusManager } from '@/index';
 
 describe('BusManager', () => {
     beforeEach(() => {
